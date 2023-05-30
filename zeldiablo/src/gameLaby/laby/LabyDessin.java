c6bd0b6dcf8e6017936b9feabd2be21b8d18cd53
CocoDietrich <corentindietrich57@gmail.com>
2023-05-30 10:57:34 +0200

    Ajout de la classe LabyDessin

---
+++ zeldiablo/src/gameLaby/laby/LabyDessin.java
@@ -0,0 +1,13 @@
+package gameLaby.laby;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import moteurJeu.Jeu;
+import moteurJeu.DessinJeu;
+
+/**
+ * permet d'afficher un jeu de type Labyrinthe
+ */
+public class LabyDessin implements DessinJeu {
+}
