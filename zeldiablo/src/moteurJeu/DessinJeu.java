8a75780ad225f5d635a4b80948c37a73d1e381fd
WashiFR <lorisweier@gmx.fr>
2023-05-31 16:14:57 +0200

    Affichage Image fonctionnel

--- zeldiablo/src/moteurJeu/DessinJeu.java
+++ zeldiablo/src/moteurJeu/DessinJeu.java
@@ -2,6 +2,8 @@ package moteurJeu;
 
 import javafx.scene.canvas.Canvas;
 
+import java.net.MalformedURLException;
+
 
 /**
  * interface pour afficher le jeu
@@ -14,6 +16,6 @@ public interface DessinJeu {
      * @param jeu jeu a afficher
      * @param canvas canvas dans lequel dessiner l'etat du jeu
      */
-    void dessinerJeu(Jeu jeu, Canvas canvas);
+    void dessinerJeu(Jeu jeu, Canvas canvas) throws MalformedURLException;
 
 }
